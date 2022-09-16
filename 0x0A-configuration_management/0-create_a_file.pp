48363faf2064c925ad5027fff006c9579e269cfe
Prulm <jaredyared83@gmail.com>
2022-09-16 21:54:20 +0300

    added the project source files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in /tmp with specific requirements
 
 file { 'holberton':
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
